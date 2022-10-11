1b9e9584304a0170e18d70a3e5cda2061ad32432
Zharkyn <Lucky_zharkyn@mail.ru>
2022-10-11 18:21:07 +0600

    разбил на компоненты

---
+++ src/Components/Otdel.js
@@ -0,0 +1,18 @@
+import Test1 from "./Test1"
+function Otdel() {
+    return <>
+        <div className="Otdel">
+            <div className="rukovodstvo">
+                <div className="odOtdeli">
+                        <p>rukovodstvo1</p>
+                        <p>text2</p>
+                </div>
+            </div>
+            <div className="tree_column">
+                <Test1 />
+            </div>
+        </div>
+    </>
+}
+
+export default Otdel
---
+++ src/Components/Test2.js
@@ -0,0 +1,41 @@
+
+function Test2() {
+    return <>
+        <div className="two">
+    <div>
+      <div className="rukovodstvo1">
+          <p>rukovodstvo</p>
+          <p>text2</p>
+      </div>
+      <div className="tree_column2">
+        <div className="odOtdeli">
+              <p>person</p>
+              <p>text2</p>
+        </div>
+        <div className="odOtdeli">
+              <p>person2</p>
+              <p>text2</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div className="rukovodstvo1">
+          <p>rukovodstvo2</p>
+          <p>text2</p>
+      </div>
+      <div className="tree_column2">
+          <div className="odOtdeli">
+                <p>person</p>
+                <p>text2</p>
+          </div>
+          <div className="odOtdeli">
+                <p>person2</p>
+                <p>text2</p>
+          </div>
+        </div>
+      </div>
+  </div>
+    </>
+}
+
+export default Test2
